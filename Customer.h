ad4639eefa79f912e1ac073ad7f08e92258a1807
Jueun Choi <cje2823@gmail.com>
2023-03-22 13:24:45 +0900

    Create RentalLog structure and define it as customerlog : This commit creates a new structure called RentalLog, which is used to store rental information such as rented movie title name, rental fee, rental days, total rental fee and total renter points. The structure is then defined as customerlog.

--- Customer.h
+++ Customer.h
@@ -6,6 +6,15 @@
 #include <vector>
 #include "Rental.h"
 
+// Define a structure to store information about a customer rental
+struct RentalLog {
+	std::vector< std::string > titleName; // Title name of the rented movie
+	std::vector< double > rentalAmount; // Rental fee charged for the rented movie
+	std::vector< int > rentalDays; // Number of days the rented movie
+	double totalRentalFee = 0.; // Total Rental Fee
+	int totalRenterPoints = 0; // Total Renter Points
+};
+
 // The customer class represents the customer of the store
 
 class Customer {
@@ -18,14 +27,17 @@ public:
 
 	// Generate a statement for the customer
 	std::string statement();
-	//add funtion to determine amounts for each rental
+	// Determine amounts for each rental
 	double getRentalFee(Rental);
-	//add funtion to determine bonus points for each rental
+	// Determine bonus points for each rental
 	int getRenterPoints(Rental);
+
 
 private:
 	std::string customerName;
 	std::vector< Rental > customerRentals;
+	// Define an alias for RentalLog structure called customerlog
+	RentalLog customerLog; 
 
 };
 
